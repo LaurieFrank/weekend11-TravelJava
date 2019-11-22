5ac14af626422b12bebe07bbb00d1456f0d2c834
Laurie Frank <laurie360@hotmail.co.uk>
2019-11-22 13:51:54 +0000

    initial commit

---
+++ src/main/java/PlaneType.java
@@ -0,0 +1,21 @@
+public enum PlaneType {
+    BOEING747(1000, 524),
+    BOEING_BBJ_MAX_7(100, 600);
+
+
+    private final int capacity;
+    private final int weight;
+
+    PlaneType(int capacity, int weight){
+        this.capacity = capacity;
+        this.weight = weight;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public int getWeight(){
+        return this.weight;
+    }
+}
